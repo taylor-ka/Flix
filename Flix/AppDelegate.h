569c7ca56efbf254ff8e7207491186997e13a9e4
Taylor Ka <taylorka@fb.com>
2019-06-26 11:01:57 -0700

    Initial Commit

---
+++ Flix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flix
+//
+//  Created by taylorka on 6/26/19.
+//  Copyright © 2019 taylorka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
